ff691b804c97cd5cb53de38ea3a656d469345d08
YanickZengaffinen <yanick.zengaffinen@gmail.com>
2019-08-16 01:26:10 +0200

    ServiceRegistry for ActivationFunctionProvider

---
+++ NeuralNetworks/ActivationFunctions/ActivationFunctionProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetworks.ActivationFunctions
+{
+    public class ActivationFunctionProvider
+    {
+        private readonly IDictionary<string, IActivationFunction> activationFunctions = new Dictionary<string, IActivationFunction>();
+
+        public void Register<T>(T function)
+            where T : IActivationFunction
+        {
+            lock(activationFunctions)
+            {
+                activationFunctions.Add(GetIdentifier(typeof(T)), function);
+            }
+        }
+
+        public T Get<T>()
+            where T : IActivationFunction
+        {
+            string id = GetIdentifier(typeof(T));
+            lock(activationFunctions)
+            {
+                if(activationFunctions.ContainsKey(id))
+                {
+                    return (T)activationFunctions[id];
+                }
+            }
+
+            return default;
+        }
+
+        private string GetIdentifier(Type t)
+        {
+            return t.FullName;
+        }
+    }
+}
---
+++ NeuralNetworks/ActivationFunctions/SigmoidFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralNetworks.ActivationFunctions
+{
+    public class SigmoidFunction : IActivationFunction
+    {
+        public double Apply(double x)
+        {
+            return 1 / (1 + Math.Pow(Math.E, -x));
+        }
+    }
+}
