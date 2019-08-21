5cb212d5e230fdce5dac667b1d4ef1d58337fe33
YanickZengaffinen <yanick.zengaffinen@gmail.com>
2019-08-21 20:37:46 +0200

    Added TrainerAPI

---
+++ TrainerAPI/API/IEpochInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainerAPI
+{
+    public interface IEpochInfo
+    {
+        int Id { get; }
+
+        double Loss { get; }
+    }
+}
---
+++ TrainerAPI/API/ITrainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TrainerAPI
+{
+    public interface ITrainer<out T>
+        where T : IEpochInfo
+    {
+        // TODO: Use async streams once .net std 2.1 is released
+        IEnumerable<T> Train(int epochs);
+    }
+}
