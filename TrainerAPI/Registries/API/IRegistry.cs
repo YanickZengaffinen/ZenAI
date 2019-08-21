96b80b2e32e07e1139421fad89ee5593cb98eb21
YanickZengaffinen <yanick.zengaffinen@gmail.com>
2019-08-21 22:27:01 +0200

    Registry in TrainerAPI

---
+++ TrainerAPI/Registries/API/IRegistry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrainerAPI.Registries
+{
+    /// <summary>
+    /// A registry of objects that derive from B
+    /// </summary>
+    public interface IRegistry<B>
+    {
+        /// <param name="alias">Optional alias instead of the default name</param>
+        void Register<T>(T entity, Type alias = null) where T : B;
+
+        T Get<T>() where T : B;
+
+        bool Unregister(Type alias);
+    }
+}
---
+++ TrainerAPI/Registries/Registry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrainerAPI.Registries
+{
+    public class Registry<B> : IRegistry<B>
+    {
+        protected IDictionary<string, B> registry = new Dictionary<string, B>();
+        public void Register<T>(T entity, Type alias = null) where T : B
+        {
+            registry.Add(GetIdentifier(alias ?? typeof(T)), entity);
+        }
+
+        public T Get<T>()
+            where T : B
+        {
+            string alias = GetIdentifier(typeof(T));
+            if (registry.ContainsKey(alias))
+            {
+                return (T)registry[alias];
+            }
+            else
+            {
+                throw new Exception($"Entity with name {alias} not found!");
+            }
+        }
+
+
+        public bool Unregister(Type alias)
+        {
+            return registry.Remove(GetIdentifier(alias));
+        }
+
+
+        private string GetIdentifier(Type t)
+            => t.FullName;
+    }
+}
